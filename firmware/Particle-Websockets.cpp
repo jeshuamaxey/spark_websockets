3793f4aa13141ba75ee2d850d926856b7e51ca66
Jeshua Maxey <jeshua.maxey@headlondon.com>
2015-09-30 16:32:46 +0100

    Name change:
    Change name to Particle-Websockets

--- firmware/Particle-Websockets.cpp
+++ firmware/Particle-Websockets.cpp
@@ -70,7 +70,7 @@
 #include "spark_wiring_string.h"
 #include "application.h"
 
-#include "Spark-Websockets.h"
+#include "Photon-Websockets.h"
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdlib.h>
